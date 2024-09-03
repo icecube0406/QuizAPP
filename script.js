86c7c0ada47a84ef1d1f43120a03823420b262d0
Stefan Schad <stefanschad46@gmail.com>
2024-09-03 21:33:10 +0200

    030902024

--- script.js
+++ script.js
@@ -66,14 +66,19 @@ let questions = [
 
     function answer(selection){
       let question = questions[currentQuestion];
-      console.log('Selected answer is ', selection)
+      console.log('Selected answer is ', selection);
       let selectedQuestionNumber = selection.slice(-1);
-      console.log('selectedQuestionNumber is ', selectedQuestionNumber)
-      console.log('Current question is ',question['right_answer'])
+      console.log('selectedQuestionNumber is ', selectedQuestionNumber);
+      console.log('Current question is ',question['right_answer']);
+
+      let idofRightAnswer = `answer_${question['right_answer']}`;
     
       if(selectedQuestionNumber == question['right_answer']) {
         console.log('Richtige Antwort!!');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
       } else {
-        console.log('Falsche Antwort!!!')
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idofRightAnswer).parentNode.classList.add('bg-success');
       }
+      document.getElementById('next-button').disabled = false;
     }
